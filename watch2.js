4631133fbf83339cbb24fafbcd4bda12869d1b0f
jinsek <jinsek@cs.washington.edu>
2024-05-05 17:27:37 -0700

    changes applied

--- watch2.js
+++ watch2.js
@@ -9,7 +9,6 @@
     //lock header when window is scrolled down
     window.onscroll = function() {
       let header = qs("header");
-      console.log(header);
       if (window.scrollY > 0) {
         header.classList.add("lock-header");
       } else {
@@ -17,20 +16,117 @@
       }
     }
 
-    //change wishlisticon when wishlist icon("♡") clicked
+    //THE HEADER START
+    let menu = id('menu');
+    let sidebar = id('sidebar');
+    let close = id("close");
+    let overlay = id("overlay");
+    let type1Sidebar = id('type1sidebar');
+    let type2Sidebar = id('type2sidebar');
+    let type3Sidebar = id('type3sidebar');
+
+    menu.classList.add(".change");
+    // change the style when putting the cursor on the menu
+
+    //click menu and open the side bar
+    menu.addEventListener('click', function (event) {
+      sidebar.style.left = '0px';
+      overlay.style.display = "block";
+      overlay.style.pointerEvents = 'auto';
+      [type1Sidebar, type2Sidebar, type3Sidebar].forEach(sidebar => {
+        sidebar.style.left = '0px';
+      });
+      event.stopPropagation();
+      document.addEventListener('click', closeSidebar);
+    });
+
+    //click and close the side bar
+    close.addEventListener('click', function () {
+      sidebar.style.left = '-300px';
+      type1Sidebar.style.left = '-300px';
+      type2Sidebar.style.left = '-300px';
+      type3Sidebar.style.left = '-300px';
+      overlay.style.display = "none";
+
+    });
+
+    let type1 = id("type1");
+    let type2 = id("type2");
+    let type3 = id("type3");
+
+
+
+    type1.addEventListener("click", function () {
+
+      hideExistSidebars(type2Sidebar, type3Sidebar);
+      toggleSidebar(type1Sidebar);
+    })
+
+    type2.addEventListener("click", function () {
+      hideExistSidebars(type1Sidebar, type3Sidebar);
+      toggleSidebar(type2Sidebar);
+    })
+
+    type3.addEventListener("click", function () {
+      hideExistSidebars(type1Sidebar, type2Sidebar);
+      toggleSidebar(type3Sidebar);
+    })
+  //THE HEADER END
+
+    //change wishlisticon when wishlist icon("â™¡") clicked
     let wishlistIcon = qs("#add-to-wishlist p");
     wishlistIcon.addEventListener('click', addToWishlist);
+
+    //change picture when right and left arrow clicked
+    let rightArrow = qs("#right-arrow p");
+    rightArrow.addEventListener('click', () => {
+      nextPicture(true);
+    });
+
+    let leftArrow = qs("#left-arrow p");
+    leftArrow.addEventListener('click', () => {
+      nextPicture(false);
+    });
+  }
+
+  function nextPicture(isRightArrow) {
+    let currentImage = qs("#img-container img");
+
+    let currentNumberIndex = currentImage.src.indexOf(".") - 1;
+    let currentNumber = parseInt(currentImage.src[currentNumberIndex]);
+
+    let nextNumber;
+    let allHR = qsa("#image-number hr");
+    if (isRightArrow) {
+      nextNumber = (currentNumber + 1) % 5;
+      nextNumber = (nextNumber % 5 === 0) ? 1 : nextNumber;
+      allHR[currentNumber % 4].classList.toggle("to-black-border");
+      allHR[currentNumber % 4].classList.toggle("to-white-border");
+    } else {
+      nextNumber = (currentNumber - 1) % 5;
+      nextNumber = (nextNumber % 5 === 0) ? 4 : nextNumber;
+      allHR[(currentNumber+2) % 4].classList.toggle("to-black-border");
+      allHR[(currentNumber+2) % 4].classList.toggle("to-white-border");
+    }
+    allHR[currentNumber-1].classList.toggle("to-black-border");
+    allHR[currentNumber-1].classList.toggle("to-white-border");
+
+    let startIndex = currentImage.src.indexOf("img/");
+    let path = currentImage.src.substring(startIndex, currentNumberIndex);
+    let newSrc = path + nextNumber + ".png";
+
+    currentImage.src = newSrc;
   }
 
   function addToWishlist() {
     let wishlistIcon = qs("#add-to-wishlist p");
     let message = gen("p");
     if (id("add-message").children.length === 0) {
-      if (wishlistIcon.textContent === "♡") {
-        wishlistIcon.textContent = "♥︎"
+      if (wishlistIcon.textContent === "â™¡") {
+        wishlistIcon.textContent = "â™¥ï¸Ž"
         message.textContent = "Added to wishlist";
       } else {
-        wishlistIcon.textContent = "♡";
+        wishlistIcon.textContent = "â™¡";
         message.textContent = "Removed from Wishlist";
       }
       setTimeout(() => {
@@ -40,6 +136,54 @@
       }
     }
 
+  // HEADER FUNCTION START
+  function toggleSidebar(subSidebar) {
+    if (subSidebar.style.left === "0px") {
+      subSidebar.style.left = "300px";
+      subSidebar.style.display = "block";
+    } else {
+      subSidebar.style.left = "0px";
+      subSidebar.style.display = "none";
+    }
+  }
+
+  function hideExistSidebars(subSidebar1, subSidebar2) {
+    [subSidebar1, subSidebar2].forEach(sidebar => {
+      if (sidebar.style.left === "300px") {
+        sidebar.style.left = "0px";
+        sidebar.style.display = "none";
+      }
+    });
+  }
+
+
+  //when click the place other than sidebar, the sidebar would be closed
+  function closeSidebar(event) {
+    let sidebar = id('sidebar');
+    let type1Sidebar = id('type1sidebar');
+    let type2Sidebar = id('type2sidebar');
+    let type3Sidebar = id('type3sidebar');
+
+    if (!sidebar.contains(event.target) && event.target !== menu
+      && !type1Sidebar.contains(event.target) && !type2Sidebar.contains(event.target)
+      && !type3Sidebar.contains(event.target)) {
+      sidebar.style.left = "-300px";
+      hideAllSidebars(type1Sidebar,type2Sidebar,type3Sidebar);
+      overlay.style.display = "none";
+      overlay.style.pointerEvents = 'none';
+
+      document.removeEventListener('click', closeSidebar);
+    }
+
+  }
+
+  function hideAllSidebars(subSidebar1, subSidebar2, subSidebar3) {
+    [subSidebar1, subSidebar2, subSidebar3].forEach(sidebar => {
+      sidebar.style.left = "-300px";
+      sidebar.style.display = "none";
+    });
+  }
+  //HEADER FUNCTION END
   function gen(element) {
     return document.createElement(element);
   }
