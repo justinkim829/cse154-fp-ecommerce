54f801db409b4e248706c85b45a801d999c388a8
JaydenWang9 <146118803+JaydenWang9@users.noreply.github.com>
2024-05-05 17:29:15 -0700

    7th

--- hearderpart/headerpart.js
+++ hearderpart/headerpart.js
@@ -4,10 +4,11 @@
 
   window.addEventListener('load', init);
 
+
+  /** initilize all the buttons in the menu */
   function init() {
 
-    //lock header when window is scrolled down
-    window.onscroll = function() {
+    window.onscroll = function () {
       let header = qs("header");
       console.log(header);
       if (window.scrollY > 0) {
@@ -23,11 +24,7 @@
     let type1Sidebar = id('type1sidebar');
     let type2Sidebar = id('type2sidebar');
     let type3Sidebar = id('type3sidebar');
-
     menu.classList.add(".change");
-    // change the style when putting the cursor on the menu
-
-    //click menu and open the side bar
     menu.addEventListener('click', function (event) {
       sidebar.style.left = '0px';
       overlay.style.display = "block";
@@ -46,14 +43,11 @@
       type2Sidebar.style.left = '-300px';
       type3Sidebar.style.left = '-300px';
       overlay.style.display = "none";
-
     });
 
     let type1 = id("type1");
     let type2 = id("type2");
     let type3 = id("type3");
-
-
 
     type1.addEventListener("click", function () {
 
@@ -71,6 +65,12 @@
       toggleSidebar(type3Sidebar);
     })
   }
+
+
+  /**
+   * make the sidebar appear and disappear
+   * @param {object} subSidebar - the sidebar of the menu
+   */
   function toggleSidebar(subSidebar) {
     if (subSidebar.style.left === "0px") {
       subSidebar.style.left = "300px";
@@ -81,6 +81,11 @@
     }
   }
 
+  /**
+   * make all the subsidebars disappear except the one I speciaficly clicked
+   * @param {object} subSidebar1 - the one that I do not click
+   * @param {object} subSidebar2 - the one that I do not click
+   */
   function hideExistSidebars(subSidebar1, subSidebar2) {
     [subSidebar1, subSidebar2].forEach(sidebar => {
       if (sidebar.style.left === "300px") {
@@ -91,7 +96,11 @@
   }
 
 
-  //when click the place other than sidebar, the sidebar would be closed
+
+  /**
+   * when click the place other than sidebar, the sidebar would be closed
+   * @param {object} event - the event that I just done.
+   */
   function closeSidebar(event) {
     let sidebar = id('sidebar');
     let type1Sidebar = id('type1sidebar');
@@ -102,14 +111,20 @@
       && !type1Sidebar.contains(event.target) && !type2Sidebar.contains(event.target)
       && !type3Sidebar.contains(event.target)) {
       sidebar.style.left = "-300px";
-      hideAllSidebars(type1Sidebar,type2Sidebar,type3Sidebar);
+      hideAllSidebars(type1Sidebar, type2Sidebar, type3Sidebar);
       overlay.style.display = "none";
       overlay.style.pointerEvents = 'none';
 
       document.removeEventListener('click', closeSidebar);
     }
-
   }
+
+  /**
+   * make all the subsidebars disappear except the one I speciaficly clicked
+   * @param {object} subSidebar1 - the subsideabr for first type of watch
+   * @param {object} subSidebar2 - the subsideabr for second type of watch
+   * @param {object} subSidebar3 - the subsideabr for third type of watch
+   */
 
   function hideAllSidebars(subSidebar1, subSidebar2, subSidebar3) {
     [subSidebar1, subSidebar2, subSidebar3].forEach(sidebar => {
@@ -156,4 +171,4 @@
     return document.querySelectorAll(selector);
   }
 
-  })();
+})();
