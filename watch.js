348ebe8bae53f6454edb0d0f6c2d3bbeea370bf1
jinsek <jinsek@cs.washington.edu>
2024-05-12 15:50:22 -0700

    merged files

--- watch.js
+++ watch.js
@@ -4,6 +4,11 @@
 
   window.addEventListener('load', init);
 
+  const CATEGORIES_MAP = new Map([
+    ['M', 3],
+    ['D', 3],
+    ['P', 3]
+  ]);
 
   function init() {
 
@@ -27,9 +32,83 @@
     }
     qs("#product-details p").addEventListener("click", displayDetailSidebar)
     qs("#sidebarfordetail .close").addEventListener("click", closeTheDetailSidebar)
+    checkClickedWatch();
 
+    let wishlistIcon = qs("#add-to-wishlist p");
+    wishlistIcon.addEventListener('click', addToWishlist);
+
+    let rightArrow = qs("#right-arrow p");
+    rightArrow.addEventListener('click', () => {
+      nextPicture(true);
+    });
+
+    let leftArrow = qs("#left-arrow p");
+    leftArrow.addEventListener('click', () => {
+      nextPicture(false);
+    });
+
+    //lock header when window is scrolled down
+    window.onscroll = function() {
+      let header = qs("header");
+      if (window.scrollY > 0) {
+        header.classList.add("lock-header");
+      } else {
+        header.classList.remove("lock-header");
+      }
+    }
   }
 
+  /**
+   * this function is used to change into the next picture when we clicked the arrow
+   * @param {boolean} isRightArrow - whether user chicked the arrow
+   */
+  function nextPicture(isRightArrow) {
+    let currentImage = qs("#img-container img");
+
+    let currentNumberIndex = currentImage.src.indexOf(".") - 1;
+    let currentNumber = parseInt(currentImage.src[currentNumberIndex]);
+
+    let nextNumber;
+    let allHR = qsa("#image-number hr");
+    if (isRightArrow) {
+      nextNumber = (currentNumber + 1) % 5;
+      nextNumber = (nextNumber % 5 === 0) ? 1 : nextNumber;
+      allHR[currentNumber % 4].classList.toggle("to-black-border");
+      allHR[currentNumber % 4].classList.toggle("to-white-border");
+    } else {
+      nextNumber = (currentNumber - 1) % 5;
+      nextNumber = (nextNumber % 5 === 0) ? 4 : nextNumber;
+      allHR[(currentNumber+2) % 4].classList.toggle("to-black-border");
+      allHR[(currentNumber+2) % 4].classList.toggle("to-white-border");
+    }
+    allHR[currentNumber-1].classList.toggle("to-black-border");
+    allHR[currentNumber-1].classList.toggle("to-white-border");
+
+    let startIndex = currentImage.src.indexOf("img/");
+    let path = currentImage.src.substring(startIndex, currentNumberIndex);
+    let newSrc = path + nextNumber + ".png";
+
+    currentImage.src = newSrc;
+  }
+
+  /** this function is used to add this product into the wishlist when click the love icon*/
+  function addToWishlist() {
+    let wishlistIcon = qs("#add-to-wishlist p");
+    let message = gen("p");
+    if (id("add-message").children.length === 0) {
+      if (wishlistIcon.textContent === "â™¡") {
+        wishlistIcon.textContent = "â™¥ï¸Ž"
+        message.textContent = "Added to wishlist";
+      } else {
+        wishlistIcon.textContent = "â™¡";
+        message.textContent = "Removed from Wishlist";
+      }
+      setTimeout(() => {
+        id("add-message").removeChild(message);
+      }, 1500);
+      id("add-message").appendChild(message);
+      }
+    }
 
   function displayDetailSidebar(event) {
     let productDetails = id("sidebarfordetail");
@@ -59,8 +138,72 @@
 
   }
 
+  // RELOAD PAGEEEEEEEEEE
+  function resetHRs() {
+    let allHRs = qsa("#image-number hr");
+    for (let i = 0; i < allHRs.length; i++) {
+      allHRs[i].classList.remove("to-white-border");
+      allHRs[i].classList.add("to-black-border");
+    }
+    allHRs[0].classList.add("to-white-border");
+    allHRs[0].classList.remove("to-blacke-border");
+  }
 
+  function checkClickedWatch() {
+    let options = qsa(".double-sidebar ul li");
+    for (let i = 0; i < options.length; i++) {
+      options[i].addEventListener('click', () => {
+        let productID = qsa(".double-sidebar ul li")[i].querySelector("p").textContent;
+        reloadPage(productID);
+      });
+    }
+  }
 
+  function changeWatchImages(productID) {
+    let productType = productID[0];
+    let productNum = parseInt(productID[1]);
+
+    let folderPath = `img/${productType}/watch${productNum}`;
+    let imagePath = folderPath + "/img1.png";
+    let currentImage = qs("#img-container img");
+    currentImage.src = imagePath;
+    currentImage.alt = imagePath;
+
+    let productNumber = qs("#product-number p");
+    productNumber.textContent = productID;
+
+    let recommendations = [];
+    let watchNums = CATEGORIES_MAP.get(productType);
+    for (let i = productNum + 1; i <= productNum + watchNums; i++) {
+      let nextWatchNum = (i % watchNums) === 0 ? watchNums : i % watchNums;
+      if (nextWatchNum !== productNum) {
+        let sameCatPath = `img/${productType}/watch${nextWatchNum}/img1.png`;
+        recommendations.push([sameCatPath, productType + nextWatchNum]);
+      }
+    }
+    for (let key of CATEGORIES_MAP.keys()) {
+      if (key !== productType) {
+        let diffCatPath = `img/${key}/watch${productNum}/img1.png`;
+        recommendations.push([diffCatPath, key + productNum]);
+      }
+    }
+    let recommendedWatches = qsa("#recommendation-list .watch-box img");
+    for (let i = 0; i < recommendedWatches.length; i++) {
+      recommendedWatches[i].src = recommendations[i][0];
+      recommendedWatches[i].alt = recommendations[i][1];
+
+      recommendedWatches[i].removeEventListener('click', recommendedWatches[i].clickHandler);
+
+      recommendedWatches[i].clickHandler = () => reloadPage(recommendedWatches[i].alt);
+      console.log(recommendedWatches[i].clickHandler);
+      recommendedWatches[i].addEventListener('click', recommendedWatches[i].clickHandler, { once: true} );
+    }
+  }
+
+  function reloadPage(productID) {
+    changeWatchImages(productID);
+    resetHRs();
+  }
 
   function openSidebar(evt) {
     let type1Sidebar = id('type1sidebar');
@@ -76,12 +219,6 @@
     evt.stopPropagation();
     document.addEventListener('click', closeSidebar);
   }
-
-
-
-
-
-
 
 
   /**
